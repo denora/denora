f4cff7a220dbb5868de0ef0e3e6f2b205c5570cd
Hal9000 <hal9000@denorastats.org>
2007-08-01 16:24:53 +0000

    Ok, stay cool, don't smash anything... committing the traditional way now...

--- src/protocol/unreal32.c
+++ src/protocol/unreal32.c
@@ -1,5 +1,4 @@
 /* Unreal 3.2.x
-/* Unreal 3.2.x
  *
  * (C) 2004-2007 Denora Team
  * Contact us at info@denorastats.org
@@ -1408,6 +1407,7 @@ void moduleAddIRCDMsgs(void) {
 
 
 /* *INDENT-ON* */
+void moduleAddIRCDCmds()
 {
     pmodule_cmd_nick(unreal_cmd_nick);
     pmodule_cmd_mode(unreal_cmd_mode);
@@ -1453,5 +1453,3 @@ int DenoraInit(int argc, char **argv)
 
     return MOD_CONT;
 }
-
- 
