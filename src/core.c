f32279a640c3064f6fa7aec41cfcb48ad7b45a85
key2peace <outsider@scarynet.org>
2012-07-15 05:13:25 +0200

    See previous

--- src/core.c
+++ src/core.c
@@ -330,8 +330,6 @@ int match_usermask(const char *mask, User * user)
 		             || match_wild_nocase(host, user->vhost));
 	}
 	free(username);
-	if (host)
-		free(host);
 	free(mask2);
 	return result;
 }
