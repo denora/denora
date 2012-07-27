3ca28cebc232131e52c0d28389ec66b4632eab86
key2peace <outsider@scarynet.org>
2012-07-27 12:55:18 +0200

    Small configure update, fixed log_ctcpversion.c to not rely on hardcoded output channel.

--- src/modules/log_ctcpversion.c
+++ src/modules/log_ctcpversion.c
@@ -5,10 +5,11 @@
 
 #include "denora.h"
 #define AUTHOR "Jobe"
-#define VERSION "1.0.1"
+#define VERSION "1.0.2"
 #define MYNAME "log_ctcpversion"
 
-#define LOGTARGET "#services"
+/* Define this if you do not want them in the logchannel */
+/* #define LOGTARGET "#services" */
 
 int DenoraInit(int argc, char **argv);
 void DenoraFini(void);
@@ -16,37 +17,44 @@ int my_version(int argc, char **argv);
 
 int DenoraInit(int argc, char **argv)
 {
-    EvtHook *hook = NULL;
-    int status;
+	EvtHook *hook = NULL;
+	int status;
 
 	USE_VAR(argc);
 	USE_VAR(argv);
 
-    hook = createEventHook(EVENT_CTCP_VERSION, my_version);
-    status = moduleAddEventHook(hook);
-    if (status != MOD_ERR_OK) {
-        /* something went wrong say something about */
-        alog(LOG_NORMAL, "[%s%s] unable to bind to EVENT_CTCP_VERSION error [%d][%s]", MYNAME, MODULE_EXT, status, ModuleGetErrStr(status));
-        return MOD_STOP;
-    }
+	hook = createEventHook(EVENT_CTCP_VERSION, my_version);
+	status = moduleAddEventHook(hook);
+	if (status != MOD_ERR_OK) {
+		/* something went wrong say something about */
+		alog(LOG_NORMAL, "[%s%s] unable to bind to EVENT_CTCP_VERSION error [%d][%s]", MYNAME, MODULE_EXT, status, ModuleGetErrStr(status));
+		return MOD_STOP;
+	}
 
-    moduleAddAuthor(AUTHOR);
-    moduleAddVersion(VERSION);
-    return MOD_CONT;
+	moduleAddAuthor(AUTHOR);
+	moduleAddVersion(VERSION);
+	return MOD_CONT;
 }
 
 void DenoraFini(void)
 {
-    alog(LOG_NORMAL, "Unloading log_ctcpversion%s", MODULE_EXT);
+	alog(LOG_NORMAL, "Unloading log_ctcpversion%s", MODULE_EXT);
 }
 
 int my_version(int argc, char **argv)
 {
-    if (argc >= 2) {
-	    /* argv[0] = nick, argv[1] = version */
-        if (stricmp(ServerName, argv[0])) {
-            denora_cmd_privmsg(s_StatServ, sstrdup(LOGTARGET), "\x02VERSION\x02 %s has version %s", argv[0], argv[1]);
-        }
-    }
-    return MOD_CONT;
+	if (argc >= 2) {
+		/* argv[0] = nick, argv[1] = version */
+		if (stricmp(ServerName, argv[0])) {
+#ifdef LOGTARGET
+			denora_cmd_privmsg(s_StatServ, sstrdup(LOGTARGET), "\x02VERSION\x02 %s has version %s", argv[0], argv[1]);
+#else
+			if (LogChannel)
+			{
+				denora_cmd_privmsg(s_StatServ, LogChannel, "\x02VERSION\x02 %s has version %s", argv[0], argv[1]);
+			}
+#endif
+		}
+	}
+	return MOD_CONT;
 }
