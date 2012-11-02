71e251cb3be3f7d40a515d8d92901a1bcf6f6395
DukePyrolator <DukePyrolator@anope.org>
2012-11-02 14:26:57 +0100

    Fixed a misplaced free() in unreaircd.c

--- src/protocol/unreal32.c
+++ src/protocol/unreal32.c
@@ -697,9 +697,6 @@ int denora_event_quit(char *source, int ac, char **av)
 			m_kill(servername, source, msg);
 		else
 			m_kill(source, source, msg);
-
-		if (msg)
-			free(msg);
 	}
 
 	return MOD_CONT;
