cd874d27364e97be73b68856260cc8630cdc3bed
DukePyrolator <DukePyrolator@anope.org>
2013-06-05 05:18:23 +0200

    fixed the 'Command out of sync' error caused by the mysql_optimize module not catching the mysql result.

--- src/modules/mysql_optimize_event.c
+++ src/modules/mysql_optimize_event.c
@@ -22,6 +22,7 @@ void DenoraFini(void);
 int DenoraInit(int argc, char **argv)
 {
 	CronEvent *evt;
+	char tables[512] = "\0";
 
 	USE_VAR(argc);
 	USE_VAR(argv);
@@ -29,29 +30,6 @@ int DenoraInit(int argc, char **argv)
 	moduleAddAuthor("Denora");
 	moduleAddVersion("1.1");
 	moduleSetType(THIRD);
-
-	evt = createCronEvent(CRON_MIDNIGHT, db_optimize);
-	addCronEvent(CRONEVENT, evt);
-
-	return MOD_CONT;
-}
-
-/**
- * Unload the module
- **/
-void DenoraFini(void)
-{
-
-}
-
-int db_optimize(const char *name)
-{
-	char tables[512] = "\0";
-	MYSQL_RES *mysql_res;
-
-	USE_VAR(name);
-
-	alog(LOG_NORMAL, "Optimizing MYSQL tables");
 
 	sprintf(tables,
 	        "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s",
@@ -70,27 +48,22 @@ int db_optimize(const char *name)
 	if (ircd->spamfilter)
 		sprintf(tables, "%s, %s", tables, SpamTable);
 
-	rdb_query(QUERY_LOW, "OPTIMIZE TABLE %s", tables);
+	alog(LOG_NORMAL, "Installing MYSQL Optimizing Event, this will run daily on your MySQL Server.");
 
-	/*
-	 * We have to catch the result of the OPTIMIZE TABLE query
-	 * or we will get an "COMMANDS OUT OF SYNC" error on all
-	 * following queries - DP, 2013
-	 *
-	 * dev.mysql.com says:
-	 *	If you get "Commands out of sync; you can't run this command now" in
-	 *	your client code, you are calling client functions in the wrong order.
-	 *	This can happen, for example, if you are using mysql_use_result()
-	 *	and try to execute a new query before you have called mysql_free_result().
-	 *	It can also happen if you try to execute two queries that return data
-	 *	without calling mysql_use_result() or mysql_store_result() in between.
-	 *
-	 * this "fix" makes denora wait until the OPTIMIZE TABLE
-	 * query is fully processed, on large tables and slow sql servers this could
-	 * take a while. a better solution is to use mysql EVENTS.
-	 */
-	mysql_res = mysql_store_result(mysql);
-	mysql_free_result(mysql_res);
+	rdb_query(QUERY_LOW, "DROP EVENT IF EXISTS `DENORA_EVENT_OPTIMIZE`");
+
+	rdb_query(QUERY_LOW, "CREATE EVENT `DENORA_EVENT_OPTIMIZE` "
+				"ON SCHEDULE EVERY 1 DAY STARTS CURRENT_DATE "
+				"DO OPTIMIZE TABLE %s", tables);
 
 	return MOD_CONT;
 }
+
+/**
+ * Unload the module
+ **/
+void DenoraFini(void)
+{
+	rdb_query(QUERY_LOW,"DROP EVENT IF EXISTS `DENORA_EVENT_OPTIMIZE`");
+}
+
