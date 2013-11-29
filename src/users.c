abbe03f7546d65fed953cdb3a8fdbdb1739ef44f
trystanscott <scott@burkee.org>
2013-11-29 11:31:31 -0700

    Removed data from uid struct on user being disconnected.

--- src/users.c
+++ src/users.c
@@ -716,6 +716,7 @@ void delete_user(User * user)
 	struct u_chanlist *c, *c2;
 	struct u_modes *m, *m2;
 	PrivMsg *p;
+	Uid *ud;
 
 	SET_SEGV_LOCATION();
 
@@ -858,6 +859,19 @@ void delete_user(User * user)
 	free(user->realname);
 	free(user->ip);
 	free(user->sqlnick);
+
+	if (ircd->p10 || (UseTS6 && ircd->ts6))
+	{
+		ud = find_uid(user->nick);
+		if (ud)
+		{
+			delete_uid(ud);
+		}
+		if (user->uid)
+		{
+			free(user->uid);
+		}
+	}
 
 	alog(LOG_EXTRADEBUG, "debug: delete_user(): update listpointers");
 	if (user->prev)
